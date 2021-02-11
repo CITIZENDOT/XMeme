5f83a8a960c82a9d13a9156cf12a215684c126c3
CITIZENDOT <appaji12368@gmail.com>
2021-02-11 16:03:25 +0530

    Bug in NodeJs installation

--- install.sh
+++ install.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-# Update apt index
-echo "sudo apt update ..."
-sudo apt update
-echo "done"
-
 # NodeJS Installation
 echo "Installing NodeJs..."
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
+curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt install nodejs build-essential
+node -v
+npm -v 
 echo "Done."
 
 # Database Installation
 echo "Installing MongoDB..."
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-sudo apt install -y mongodb-org
+sudo apt-get install gnupg
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
 sudo systemctl start mongod
 sudo systemctl status mongod
 sudo systemctl enable mongod
